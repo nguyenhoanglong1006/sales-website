54e1d3bbcc67de2feda0013d125000d1e1a28db2
nguyenhoanglong1006 <nguyenhlong1006@gmail.com>
2024-09-12 00:51:34 +0700

    update source

---
+++ webappserver/config/database.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//    "hostname": "27.71.234.75",
+//    "username": "chips_store",
+//    "password": "6dKpuGsg",
+//    "database": "chips_store",
+//    "port": "5432",
+//    "ssl":false
+// }
+
+module.exports = {
+   "hostname": "127.0.0.1",
+   "username": "postgres",
+   "password": "100601",
+   "database": "chips_store",
+   "port": "5432",
+   "ssl":false
+}
